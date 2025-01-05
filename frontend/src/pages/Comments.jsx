039fe73a049594b623651cbe4342db4b942c840b
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-01-05 19:20:16 +0300

    working on frontend

--- frontend/src/pages/Comments.jsx
+++ frontend/src/pages/Comments.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 const Comments = () => {
-  return <div>Comments</div>;
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/comments");
+        setComments(response.data);
+      } catch (error) {
+        toast.error("Failed to fetch comments");
+      }
+    };
+    //fetchComments();
+    console.log("fetching comments");
+  }, []);
+  return (
+    <>
+      <ToastContainer />
+      <div className="comments">
+        <div className="comment">
+          <h2>Comments</h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Comments;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/profile");
+        setProfile(response.data);
+      } catch (error) {
+        toast.error("Failed to fetch profile");
+      }
+    };
+    //fetchProfile();
+    console.log("fetching profile");
+  }, []);
+  return (
+    <>
+      <ToastContainer />
+      <div className="profile">
+        <div className="bio">
+          <h2>Username + Photo</h2>
+        </div>
+        <div className="profile-post">
+          <h2>Posts</h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
