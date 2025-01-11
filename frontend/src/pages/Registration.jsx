d9429c79dcb9c2f6031c63b4c05a88942d9ade03
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-01-11 12:10:37 +0300

    register page

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -3,17 +3,24 @@ import Form from "react-bootstrap/Form";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useFormik } from "formik";
-// import registrationSchema from "../schemas/registrationSchema.js";
+import { FaFacebook } from "react-icons/fa";
+import "../styles/Registration.css";
+import RegistrationValidation from "../schemas/registrationSchema.js";
 
 const Registration = () => {
+  const image =
+    "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png";
   const formik = useFormik({
     initialValues: {
       username: "",
+      fullname: "",
       email: "",
+      profilePhoto: "",
+      bio: "",
       password: "",
       confirmPassword: "",
     },
-    // validationSchema: registrationSchema,
+    validationSchema: RegistrationValidation,
   });
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,61 +28,109 @@ const Registration = () => {
   };
   return (
     <>
-      <Form>
+      <Form className="registration-form">
+        <img src={image} alt="instagram" />
+        <h5
+          style={{ textAlign: "center", color: "gray", marginBottom: "20px" }}
+        >
+          Sign up to see photos and videos from your friends.
+        </h5>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "20px",
+          }}
+        >
+          <Button variant="primary" className="facebook-btn">
+            {" "}
+            <FaFacebook style={{ fontSize: "20px", marginRight: "10px" }} />
+            <b>Log in with Facebook</b>
+          </Button>
+        </div>
+        <h5
+          style={{ color: "gray", textAlign: "center", marginBottom: "20px" }}
+        >
+          OR
+        </h5>
         <Form.Group className="mb-3">
-          <Form.Label>
-            Username<span style={{ color: "red" }}>*</span>
-          </Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter email"
+            placeholder="Username"
             name="username"
             value={formik.values.username}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            style={{ backgroundColor: "rgb(242, 242, 242)" }}
           />
           {formik.touched.username && formik.errors.username ? (
             <small style={{ color: "red" }}>{formik.errors.username}</small>
           ) : null}
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label>
-            Email address<span style={{ color: "red" }}>*</span>
-          </Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Full Name"
+            name="fullname"
+            value={formik.values.fullname}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            style={{ backgroundColor: "rgb(242, 242, 242)" }}
+          />
+          {formik.touched.fullname && formik.errors.fullname ? (
+            <small style={{ color: "red" }}>{formik.errors.fullname}</small>
+          ) : null}
+        </Form.Group>
+        <Form.Group className="mb-3">
           <Form.Control
             type="email"
-            placeholder="Enter email"
+            placeholder="Email"
             name="email"
             value={formik.values.email}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            style={{ backgroundColor: "rgb(242, 242, 242)" }}
           />
           {formik.touched.email && formik.errors.email ? (
             <small style={{ color: "red" }}>{formik.errors.email}</small>
           ) : null}
         </Form.Group>
         <Form.Group controlId="formFile" className="mb-3">
-          <Form.Label>Profile Photo</Form.Label>
-          <Form.Control type="file" />
+          <Form.Label>
+            Profile Photo <span style={{ color: "gray" }}>(optional)</span>
+          </Form.Label>
+          <Form.Control
+            type="file"
+            name="profilePhoto"
+            value={formik.values.profilePhoto}
+            onChange={formik.handleChange}
+          />
           {formik.touched.profilePhoto && formik.errors.profilePhoto ? (
             <small style={{ color: "red" }}>{formik.errors.profilePhoto}</small>
           ) : null}
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
-        </Form.Group>
-        <Form.Group className="mb-3">
           <Form.Label>
-            Confirm Password<span style={{ color: "red" }}>*</span>
+            Bio <span style={{ color: "gray" }}>(optional)</span>
           </Form.Label>
           <Form.Control
+            as="textarea"
+            rows={3}
+            style={{ backgroundColor: "rgb(242, 242, 242)" }}
+            name="bio"
+            value={formik.values.bio}
+            onChange={formik.handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
             type="password"
-            placeholder="Password"
+            placeholder="Confirm Password"
             name="confirmPassword"
             value={formik.values.confirmPassword}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            style={{ backgroundColor: "rgb(242, 242, 242)" }}
           />
           {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
             <small style={{ color: "red" }}>
@@ -84,9 +139,6 @@ const Registration = () => {
           ) : null}
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label>
-            Password<span style={{ color: "red" }}>*</span>
-          </Form.Label>
           <Form.Control
             type="password"
             placeholder="Password"
@@ -94,16 +146,41 @@ const Registration = () => {
             value={formik.values.password}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            style={{ backgroundColor: "rgb(242, 242, 242)" }}
           />
           {formik.touched.password && formik.errors.password ? (
             <small style={{ color: "red" }}>{formik.errors.password}</small>
           ) : null}
         </Form.Group>
 
-        <Button variant="primary" type="submit" onClick={handleSubmit}>
-          Submit
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={handleSubmit}
+          style={{ width: "100%" }}
+        >
+          Sign Up
         </Button>
+        <h5
+          style={{
+            textAlign: "center",
+            color: "gray",
+            marginBottom: "20px",
+            marginTop: "20px",
+          }}
+        >
+          By signing up, you agree to our <b>Terms & Privacy Policy.</b>
+        </h5>
       </Form>
+
+      <div className="login">
+        <p>
+          Have an account?{" "}
+          <a href="/" style={{ textDecoration: "none" }}>
+            Log in
+          </a>
+        </p>
+      </div>
     </>
   );
 };
--- frontend/src/schemas/registrationSchema.js
+++ frontend/src/schemas/registrationSchema.js
@@ -1,1 +1,31 @@
 import * as Yup from "yup";
+
+const RegistrationValidation = Yup.object({
+  username: Yup.string()
+    .min(3, "Must be at least 3 characters")
+    .max(15, "Must be 15 characters or less")
+    .required("Username is required"),
+  fullname: Yup.string()
+    .min(6, "Must be at least 6 characters")
+    .max(15, "Must be 15 characters or less")
+    .required("Full name is required"),
+  email: Yup.string()
+    .min(3)
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8)
+    .required("Enter a password")
+    .matches(/[0-9]/, "Password requires a number")
+    .matches(/[a-z]/, "Password requires a lowercase letter")
+    .matches(/[A-Z]/, "Password requires an uppercase letter")
+    .matches(
+      /[$&+,:;=?@#|'<>.^*()%!-]/,
+      "Password requires a special character"
+    ),
+  confirmPassword: Yup.string()
+    .required("Enter a password")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
+
+export default RegistrationValidation;
