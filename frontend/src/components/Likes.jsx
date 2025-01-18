5c9c50d239a0c7e84600939187083381d160fa5d
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-01-18 11:19:28 +0300

    adding likes functionality

--- frontend/src/components/Likes.jsx
+++ frontend/src/components/Likes.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
 
 const Likes = () => {
-  return <div>Likes</div>;
+  const [likes, setLikes] = useState(0);
+  const handleClick = () => {
+    setLikes(likes + 1);
+  };
+  return (
+    <>
+      <div>
+        <FaHeart onClick={handleClick} />
+        <p> {likes} likes</p>
+      </div>
+    </>
+  );
 };
 
 export default Likes;
