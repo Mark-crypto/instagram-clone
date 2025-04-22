e24df4db8d024def39cb74c21ff2f417ff0d148b
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-04-22 15:54:15 +0300

    socket.io

--- frontend/src/pages/Message.jsx
+++ frontend/src/pages/Message.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
 
 const Message = () => {
-  return <div>Message</div>;
+  const [name, setName] = useState("");
+  const [roomId, setRoomId] = useState("");
+
+  const joinRoom = () => {
+    if (name !== "" && roomId !== "") {
+      socket.emit("join_room", roomId);
+    }
+  };
+  return (
+    <>
+      <h3>Join a room</h3>
+      <input
+        type="text"
+        name="name"
+        placeholder="Mark...."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        name="roomId"
+        placeholder="Room ID.."
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <button type="button" onClick={joinRoom}>
+        Join a chat
+      </button>
+    </>
+  );
 };
 
 export default Message;
--- server/app.js
+++ server/app.js
@@ -1,35 +1,45 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
 
 app.use(cors());
 
-const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-
-const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
-
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
+  console.log(`User connected with id: ${socket.id}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    console.log(`User with id ${socket.id} joined room ${data}`);
   });
-
-  socket.on("message", (data) => {
-    console.log(data);
-    socket.broadcast.emit("receive_message", data);
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
   });
 });
 
 server.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
+
+// io.on("connection", (socket) => {
+//   console.log(`User connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+
+//   socket.on("message", (data) => {
+//     console.log(data);
+//     socket.broadcast.emit("receive_message", data);
+//   });
+// });
