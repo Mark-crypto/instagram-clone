8f111ea7460ee1d20ec25d4cd70a0d9aed4b5c98
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-05-05 23:27:08 +0300

    removing .env

--- server/database.js
+++ server/database.js
@@ -1,14 +1,5 @@
-import mysql from "mysql2";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  connectionLimit: 10,
-});
-
-export default connection;
-// This snippet creates a connection to the database using the mysql2 package and exports it for use in other files. The connection details are retrieved from environment variables using the dotenv package.
+const connection = mongoose.createConnection(process.env.MONGO_URI);
